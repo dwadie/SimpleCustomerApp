00b2c9f960e546835aea74a8a42f2db94397b9e7
duraid.wadie <duraid.wadie@ideagenplc.com>
2020-01-09 10:17:31 +0000

    commit 2

--- SimpleCustomerApp/src/com/room/sample/servlet/InsertCustomerServlet.java
+++ SimpleCustomerApp/src/com/room/sample/servlet/InsertCustomerServlet.java
@@ -12,7 +12,9 @@ import com.room.sample.view.Customer;
 
 public class InsertCustomerServlet extends HttpServlet{
 
+
     
+
     private static final long serialVersionUID = 1L;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response){
