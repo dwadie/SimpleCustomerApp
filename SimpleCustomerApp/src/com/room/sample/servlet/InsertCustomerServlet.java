ffb23715e39965a083205875ad5bf2be71cbbc60
duraid.wadie <duraid.wadie@ideagenplc.com>
2020-01-09 10:27:57 +0000

    commit 6

--- SimpleCustomerApp/src/com/room/sample/servlet/InsertCustomerServlet.java
+++ SimpleCustomerApp/src/com/room/sample/servlet/InsertCustomerServlet.java
@@ -13,7 +13,7 @@ import com.room.sample.view.Customer;
 public class InsertCustomerServlet extends HttpServlet{
 
 
-    // commit 5
+    // commit code 6
 
     private static final long serialVersionUID = 1L;
 
